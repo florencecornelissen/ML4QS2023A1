0e3f5ec28b1380638a0d554042f40c0d8c45d808
florencecornelissen <f.y.cornelissen@student.vu.nl>
2023-06-08 21:39:38 +0200

    Added preprocessing etc data Flo

---
+++ edittimesnoise.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import os
+
+maps = ['FloSquads/', 'FloLunges/', 'FloJumpingJacks/', 'FloLegRaises/', 'FloCrunches/', 'FloPushUps/']
+
+# Specify the directory containing the CSV files
+directory = '/Users/florencecornelissen/Documents/VU/ML4QS/ML4QS2023A1/Python3Code/datasets/exercises/'
+
+# Create an empty dictionary to store the DataFrames
+dfs = {}
+dfsall = {}
+
+for map in maps:
+    path = os.path.join(directory, map)
+    
+    # Loop through all files in the directory
+    for filename in os.listdir(path):
+        if filename == 'Accelerometer.csv':  # Check if the file is a CSV file
+            file_path = os.path.join(path, filename)  # Use 'path' instead of 'directory'
+            
+            # Read the CSV file into a pandas DataFrame
+            df = pd.read_csv(file_path)
+            
+            # Assign a name to the DataFrame (e.g., using the filename without the extension)
+            name = filename[:-4] + map[:-1] # Remove the '.csv' extension
+            dfsall[name] = df   
+
+timesdf = pd.read_csv('/Users/florencecornelissen/Documents/VU/ML4QS/ML4QS2023A1/Python3Code/datasets/exercises/timeflo.csv')
+
+indicesbegin = [234, 193, 115, 186, 199, 360]
+begintimes = []
+for df1 in dfsall.values():
+    if indicesbegin == []:
+        break
+    begintimes.append(df1.at[indicesbegin[0], 'Time (s)'])
+    indicesbegin.pop(0)
+
+indicesend = [2540, 3841, 3136, 2647, 3961, 1975]
+endtimes = []
+for df1 in dfsall.values():
+    if indicesend == []:
+        break
+    endtimes.append(df1.at[indicesend[0], 'Time (s)'])
+    indicesend.pop(0)
+
+labels = ['squad', 'lunge', 'jumpingjack', 'legraise', 'crunch', 'pushup']
+
+i=0
+for i in timesdf.index:
+    if begintimes == []:
+        break
+    if (i%2 == 0):
+        timesdf.loc[i, 'experiment_time'] = begintimes[0]
+        begintimes.pop(0)
+        timesdf.at[i, 'label'] = labels[0]
+    else:
+        timesdf.loc[i, 'experiment_time'] = endtimes[0]
+        endtimes.pop(0)
+        timesdf.at[i, 'label'] = labels[0]
+        labels.pop(0)
+
+if endtimes:
+    timesdf.loc[i, 'experiment_time'] = endtimes[0]
+    timesdf.at[i, 'label'] = labels[0]
+
+# Create a pivot table indexed by label
+pivottimes  = timesdf.pivot_table(index='label', columns='event', values='experiment_time')
+
+pivottimes.to_csv('/Users/florencecornelissen/Documents/VU/ML4QS/ML4QS2023A1/Python3Code/datasets/exercises/timeflo.csv', index=True)
---
+++ preprocessdataflo.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import os
+
+maps = ['FloSquads', 'FloLunges', 'FloJumpingJacks', 'FloLegRaises', 'FloCrunches', 'FloPushUps']
+
+
+# Specify the directory containing the CSV files
+directory = '/Users/florencecornelissen/Documents/VU/ML4QS/ML4QS2023A1/Python3Code/datasets/exercises/FloSquads/'
+
+# Create an empty dictionary to store the DataFrames
+dataframes = {}
+
+# Loop through all files in the directory
+for filename in os.listdir(directory):
+    if filename.endswith('.csv'):  # Check if the file is a CSV file
+        file_path = os.path.join(directory, filename)
+            
+        # Read the CSV file into a pandas DataFrame
+        df = pd.read_csv(file_path)
+            
+        # Assign a name to the DataFrame (e.g., using the filename without the extension)
+        name = filename[:-4]# Remove the '.csv' extension
+        dataframes[name] = df
+            
+        # Process the DataFrame or perform desired operations
+        # For example, you can print the contents of the DataFrame
+
+maps = ['FloLunges/', 'FloJumpingJacks/', 'FloLegRaises/', 'FloCrunches/', 'FloPushUps/']
+
+# Specify the directory containing the CSV files
+directory = '/Users/florencecornelissen/Documents/VU/ML4QS/ML4QS2023A1/Python3Code/datasets/exercises/'
+
+# Create an empty dictionary to store the DataFrames
+additionaldfs = {}
+
+for map in maps:
+    path = os.path.join(directory, map)
+    
+    # Loop through all files in the directory
+    for filename in os.listdir(path):
+        if filename.endswith('.csv'):  # Check if the file is a CSV file
+            file_path = os.path.join(path, filename)  # Use 'path' instead of 'directory'
+            
+            # Read the CSV file into a pandas DataFrame
+            df = pd.read_csv(file_path)
+            
+            # Assign a name to the DataFrame (e.g., using the filename without the extension)
+            name = filename[:-4] + map[:-1] # Remove the '.csv' extension
+            additionaldfs[name] = df
+            
+            # Process the DataFrame or perform desired operations
+            # For example, you can print the contents of the DataFrame
+
+# Concatenate all the dataframes of the same measurement type in one dataframe
+files = []
+for filename in os.listdir('/Users/florencecornelissen/Documents/VU/ML4QS/ML4QS2023A1/Python3Code/datasets/exercises/FloCrunches/'):
+    if filename.endswith('.csv'):
+        files.append(filename[:-4])
+
+combineddf = {}
+
+
+for df in dataframes.keys():
+    for df2 in additionaldfs.keys():
+        if df in df2:
+            combineddf[df] = pd.concat([dataframes[df], additionaldfs[df2]])
+            dataframes[df] = combineddf[df]
+            # print(combineddf[df])
+
+dataframes['Accelerometer'].rename(columns={'Time (s)': 'timestep', 'X (m/s^2)': 'x', 'Y (m/s^2)': 'y', 'Z (m/s^2)': 'z'}, inplace=True)
+dataframes['Barometer'].rename(columns={'Time (s)': 'timestep', 'X (hPa)': 'x'}, inplace=True)
+dataframes['Gyroscope'].rename(columns={'Time (s)': 'timestep', 'X (rad/s)': 'x', 'Y (rad/s)': 'y', 'Z (rad/s)': 'z'}, inplace=True)
+dataframes['Linear_Accelerometer'].rename(columns={'Time (s)': 'timestep', 'X (m/s^2)': 'x', 'Y (m/s^2)': 'y', 'Z (m/s^2)': 'z'}, inplace=True)
+dataframes['Location'].rename(columns={'Time (s)': 'timestep', 'Latitude (°)': 'latitude', 'Longitude (°)': 'longitude', 'Height (m)': 'h', 'Velocity (m/s)': 'vel', 'Direction (°)': 'dir', 'Horizontal Accuracy (m)': 'hor_acc', 'Vertical Accuracy (°)': 'ver_acc'}, inplace=True)
+dataframes['Magnetometer'].rename(columns={'Time (s)': 'timestep', 'X (µT)': 'x', 'Y (µT)': 'y', 'Z (µT)': 'z'}, inplace=True)
+dataframes['Proximity'].rename(columns={'Time (s)': 'timestep', 'Distance (cm)': 'dis'}, inplace=True)
+dataframes['time'].rename(columns={'experiment time': 'experiment_time', 'system time': 'system_time', 'system time text': 'system_text_time'}, inplace=True)
+dataframes['time'] = dataframes['time'].drop(columns=['system_time', 'system_text_time'], axis=1)
+
+# Save to dataframes as csv in the datasets folder
+for df in dataframes.keys():
+    dataframes[df].to_csv('/Users/florencecornelissen/Documents/VU/ML4QS/ML4QS2023A1/Python3Code/datasets/exercises/' + df + 'flo.csv', index=False)
