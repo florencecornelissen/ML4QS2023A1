4dcae8e81385baae95d483a0e21336c6667704ce
florencecornelissen <f.y.cornelissen@student.vu.nl>
2023-06-14 11:31:39 +0200

    Added datasets Flo and Ivo

--- Python3Code/preprocessdata.py
+++ Python3Code/preprocessdata.py
@@ -1,4 +1,4 @@
-
+from preprocessalldata import preprocessdatatotal
 # names = ['Ivo', 'Joost', 'Flo']
 
 # for name in names:
@@ -10,9 +10,10 @@ indicesendFlo = [2540, 3841, 3136, 2647, 3961, 1975]
 indicesbeginIvo = [192, 183, 400, 346, 288, 337]
 indicesendIvo = [4086, 4299, 3200, 4279, 2357, 2725]
 
-participants = ['Ivo','Flo']
+participants = [{'name': 'Ivo', 'indicesbegin': indicesbeginIvo, 'indicesend': indicesendIvo},
+               {'name': 'Flo', 'indicesbegin': indicesbeginFlo, 'indicesend': indicesendFlo}]
 
 for participant in participants:
-    preprocessdatatotal(participant, indicesbegin = 'indicesbegin'+participant, indicesend = 'indicesend'+participant')
+    preprocessdatatotal(participant['name'], participant['indicesbegin'], participant['indicesend'])
 
         
